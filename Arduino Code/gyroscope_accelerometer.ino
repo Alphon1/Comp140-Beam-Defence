476bd3aa478b4f07642a1e7a1229f5bf0f211d9c
Alex Williamson <alex_williamson@rocketmail.com>
2020-04-26 13:17:05 +0100

    Included the Arduino code

---
+++ Arduino Code/gyroscope_accelerometer.ino
@@ -0,0 +1,37 @@
+#include<Wire.h>
+const int MPU=0x68; 
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+}
+
+char Input = 0;
+void loop(){
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();  
+  GyX=Wire.read()<<8|Wire.read();  
+  GyY=Wire.read()<<8|Wire.read();  
+  GyZ=Wire.read()<<8|Wire.read();  
+
+  if (Serial.available() > 0) 
+  {
+    Input = Serial.read();
+    if (Input = 'I')
+    {
+      Serial.print(AcY); Serial.print(",");
+      Serial.print(AcZ); Serial.print(",");
+      Serial.print(GyY); Serial.println(" "); 
+    }
+  }
+}
